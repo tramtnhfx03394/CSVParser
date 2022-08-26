e0f84d7221aac4ef7e121963634ba9c7d247e044
tramtnhfx03394 <tramtnhfx03394@funix.edu.vn>
2022-08-26 08:08:34 +0700

    chore: refactor

--- src/main/java/com/csvparser/Parser.java
+++ src/main/java/com/csvparser/Parser.java
@@ -20,19 +20,22 @@ public class Parser {
     public static void main(String[] args) throws Exception {
         ConfigurableApplicationContext context = SpringApplication.run(Parser.class, args);
         if (args.length > 2) {
-            Map<String, Long> m = calculateScoreFromInput(args[0]);
-
-            AthleteScoreRepository repository = context.getBean(AthleteScoreRepository.class);
-
-            List<AthleteScore> athleteScoreList = m.entrySet().stream().map(x -> AthleteScore.builder()
-                    .athleteName(toPascalCase(x.getKey()))
-                    .totalScore(x.getValue())
-                    .build()).collect(Collectors.toList());
-
-            repository.saveAll(athleteScoreList);
-
-            outputStatsToFile(args[2], m);
+            Map<String, Long> data = calculateScoreFromInput(args[0]);
+            insertToDB(context, data);
+            outputStatsToFile(args[2], data);
         }
+    }
+
+    private static void insertToDB(ConfigurableApplicationContext context, Map<String, Long> data) {
+        AthleteScoreRepository repository = context.getBean(AthleteScoreRepository.class);
+
+        List<AthleteScore> athleteScoreList = data.entrySet().stream().map(x -> AthleteScore.builder()
+                .athleteName(toPascalCase(x.getKey()))
+                .totalScore(x.getValue())
+                .build()).collect(Collectors.toList());
+
+        repository.saveAll(athleteScoreList);
+
     }
 
     // calculate each athlete total score from input
@@ -88,6 +91,7 @@ public class Parser {
             if (str.charAt(i) == ' ') {
                 continue;
             }
+            //if previous char is a space then char i should be uppercase
             if (str.charAt(i - 1) == ' ') {
                 res.add(Character.toUpperCase(str.charAt(i)));
                 continue;
